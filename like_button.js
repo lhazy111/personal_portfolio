1f796f0383a5706dbb03be7db9d010b3db5f39da
lhazy111 <lhazy111@googlemail.com>
2020-05-27 16:48:53 +0100

    images added

---
+++ like_button.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
+ReactDOM.render(e(LikeButton), document.getElementById('like_button_container'));
